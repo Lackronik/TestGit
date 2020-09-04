3b13772366e023e6d93ed0581191da19fdb9805c
Alex Petr <lackronik@gmail.com>
2020-09-04 13:57:37 +0300

    On branch master  Your branch is up to date with 'first/master'.
     Changes to be committed:
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,18 @@ int main()
 	if (2>1) then
 	{
 	cout << "Hello World!" << endl; //в Си: printf("Hello, World!\n");
-	return 0;
 	}
+
+int a = 10;
+int b = 20;
+
+a += b;
+b -= a;
+
+cout << "result on a:" << a << "   result of b:" << b << endl;
+
+
+
+return 0;
 }
 
