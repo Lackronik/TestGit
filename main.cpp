7b58790c767d171982de9980b03db909a3ea4e70
Alex Petr <lackronik@gmail.com>
2020-09-04 11:52:52 +0300

    On branch master  Changes to be committed: 	new file:   README 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
+	if (2>1) then
+	{
 	cout << "Hello World!" << endl; //в Си: printf("Hello, World!\n");
 	return 0;
+	}
 }
 
